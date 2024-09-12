c69dc61b41d71ce5d21ad8857ad8c1904a0292be
ian-reedy <IanReedy50@gmail.com>
2024-09-12 14:12:21 -0400

    save

---
+++ Chap4/4-1. pizzas.py
@@ -0,0 +1,4 @@
+pizzas = ['buffalo','pepperoni','stuffed crust'] 
+for pizza in pizzas:
+    print(f"I love {pizza.title()} pizza") 
+print("I really like pizza. Especially buffalo pizza. It is my favorite just because I love buffalo sauce. " + "\n I really love pizza")
---
+++ Chap4/4-2. animals.py
@@ -0,0 +1,12 @@
+anims = ['dog','cat','hyeana']
+for anim in anims:
+    if anim == 'dog':
+        print("A " + anim + " is a mans best friend.")
+    elif anim == 'cat':
+        print("My " + anim + " isn't very social.")
+    elif anim == 'hyeana':
+        print("A " + anim + " is a very dangerous animal.")
+    
+
+     
+     
