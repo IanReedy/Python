316a4a9c6882abf73c02da36ed79a64502af9101
ian-reedy <IanReedy50@gmail.com>
2024-10-01 09:10:51 -0400

    save

---
+++ Chap1/01-01.python.org.py
@@ -0,0 +1,8 @@
+"""
+I think an interesting part of python.org is its "Our Success Stories" section. It is cool seeing what people using the same language I do and school and it gives me something
+to look forward to in the future knowing I might be able to make something like that.
+
+
+
+
+"""
--- Chap1/1-3Infinite.py
+++ Chap1/1-3Infinite.py
@@ -1,6 +1,8 @@
 '''
 If i had infinite programming skills I would like to program a game from scratch. I'm thinking of doing game and sim because I think it sounds fun. Also I might 
-want to create a few apps that would be useful to me. 
+want to create a few apps that would be useful to me. For example, a really easy one would be on either on phone or pc, that could count clicks or taps on the screen. 
+Another app that sound like fun to make is something that tracks keys people press. I'm not sure how hard an antivirus would be but making one of those would be nice too.
+
 
 
 
